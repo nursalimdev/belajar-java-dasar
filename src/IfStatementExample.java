c549eb0255cea5bcb0551f5708b51004935bd4d3
nursalimdev <nursalimdev123@gmail.com>
2023-01-26 20:04:33 +0700

    commit

---
+++ src/IfStatementExample.java
@@ -0,0 +1,12 @@
+public class IfStatementExample {
+    public static void main(String[] args) {
+        var nilai = 70;
+        var absen = 90;
+
+        if(nilai >= 75 && absen >= 75) {
+            System.out.println("Selamat Anda lulus");
+        }else{
+            System.out.println("Anda gagal");
+        }
+    }
+}
