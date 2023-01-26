1fc8555d65d6b030f75cbcf753cfb2022d279a41
nursalimdev <nursalimdev123@gmail.com>
2023-01-27 06:28:13 +0700

    commit

---
+++ src/DoWhileLoopExample.java
@@ -0,0 +1,9 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        var counter = 1;
+        do {
+            System.out.println("Perulangan ke - " + counter);
+            counter++;
+        }while (counter <= 10);
+    }
+}
---
+++ src/ForLoopExample.java
@@ -0,0 +1,26 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+//        perulangan tanpa henti
+//        for (;;){
+//            System.out.println("Perulangan tanpa henti");
+//        }
+
+//        perulangan dengan kondisi
+//        var counter = 1;
+//        for(;counter <= 10;){
+//            System.out.println("Perulangan ke - " + counter);
+//            counter++;
+//        }
+
+//        perulangan dengan init statement
+//        for(var counter = 1; counter <= 10;){
+//            System.out.println("Perulangan ke - " + counter);
+//            counter++;
+//        }
+
+//        perulangan dengan post statement
+        for(var counter = 1; counter <= 10; counter++){
+            System.out.println("Perulangan ke - " + counter);
+        }
+    }
+}
